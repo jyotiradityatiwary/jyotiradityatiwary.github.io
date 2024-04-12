0e0023d1629eada56687771fc92cdb3a1d4dfd1b
Jyotiraditya Tiwary <78427496+jyotiradityatiwary@users.noreply.github.com>
2024-04-12 21:19:44 +0530

    Upload syncony next preview files

---
+++ syncony-next-preview/_next/static/chunks/app/(pre-login)/layout-29576e2060c1d8cb.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[829],{6902:function(n,a,e){Promise.resolve().then(e.t.bind(e,9678,23))},9678:function(n){n.exports={style:{fontFamily:"'__Inter_aaf875', '__Inter_Fallback_aaf875'",fontStyle:"normal"},className:"__className_aaf875"}}},function(n){n.O(0,[971,69,744],function(){return n(n.s=6902)}),_N_E=n.O()}]);
